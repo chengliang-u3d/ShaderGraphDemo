804bc767ea3b861fc0550311d9298294324b741a
ChengLiang <chengliang@unity3d.com>
2018-03-29 23:00:06 +0800

    -add daylight sample

---
+++ Assets/Scripts/AccessProperty.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class AccessProperty : MonoBehaviour {
+
+    Material curMaterial;
+
+    Light curLight;
+
+	// Use this for initialization
+	void Start () {
+        curMaterial = GetComponent<MeshRenderer>().sharedMaterials[0];
+        //Color tmp = curMaterial.GetColor("Color_CB4C3EA6");
+        curMaterial.SetColor("Color_CB4C3EA6", Color.red);
+
+        curLight = GameObject.Find("Directional Light").GetComponent<Light>();
+
+        curMaterial.SetVector ("Vector3_6D9D3964", curLight.transform.forward);
+
+        curMaterial.SetFloat("Vector1_54A6A2AC", curLight.intensity);
+
+        //curMaterial.Set
+        //Vector3_6D9D3964
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        curMaterial.SetVector("Vector3_6D9D3964", -curLight.transform.forward);
+
+        curMaterial.SetFloat("Vector1_54A6A2AC", curLight.intensity);
+
+	}
+}
---
+++ Assets/Scripts/AutoIntensity.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoIntensity : MonoBehaviour {
+
+    Transform sunTrans;
+    Light curLight; 
+
+	// Use this for initialization
+	void Start () {
+        sunTrans = GetComponent<Transform>();
+        curLight = GetComponent<Light>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        sunTrans.Rotate(Time.deltaTime * 20, 0, 0);
+
+        if (sunTrans.rotation.eulerAngles.x < 0 || 
+            sunTrans.rotation.eulerAngles.x > 180 )
+        {
+            curLight.intensity = 0;
+        }
+        else
+        {
+            curLight.intensity = 1;
+        }
+	}
+}
